feaee4d0fe8437cc2e31f64c3d457735b6ef448d
silly sausage <vinbest777@gmail.com>
2024-05-08 01:10:37 -0700

    budget check for edit item

--- WindowsFormsApp1/Form5.cs
+++ WindowsFormsApp1/Form5.cs
@@ -37,34 +37,36 @@ namespace WindowsFormsApp1
                 item.Description = textBox3.Text;
                 DBConnect.EditItem(id, item.ItemName, (decimal)item.Cost, item.Description);
                 Program.mainDisplay.LoadData();
+                
+                if (DBConnect.displayBudget() < (DBConnect.displayTotal()))
+                {
+                    bool form3open = false;
+                    FormCollection fc = Application.OpenForms;
+
+                    foreach (Form frm in fc)
+                    {
+                        if (frm.Name == "Form3")
+                        {
+                            form3open = true;
+                        }
+                    }
+                    if (!form3open)
+                    {
+                        DialogResult resultBudget = MessageBox.Show("Total spending exceeds budget, would you like to update your budget?.", "Warning", MessageBoxButtons.YesNo);
+                        if (resultBudget == DialogResult.Yes)
+                        {
+                            Form4 f4 = new Form4();
+                            f4.Show();
+                        }
+                    }
+                }
                 this.Close();
             }
             else
             {
                 MessageBox.Show("Invalid or missing values.", "Warning");
             }
-            if (DBConnect.displayBudget() < (DBConnect.displayTotal()))
-            {
-                bool form3open = false;
-                FormCollection fc = Application.OpenForms;
-
-                foreach (Form frm in fc)
-                {
-                    if (frm.Name == "Form3")
-                    {
-                        form3open = true;
-                    }
-                }
-                if (!form3open)
-                {
-                    DialogResult resultBudget = MessageBox.Show("Total spending exceeds budget, would you like to update your budget?.", "Warning", MessageBoxButtons.YesNo);
-                    if (resultBudget == DialogResult.Yes)
-                    {
-                        Form4 f4 = new Form4();
-                        f4.Show();
-                    }
-                }
-            }
+            
         }
     }
 }
